f9d7de0dc7391dc0201445374c2b751daa37306a
ops99 <ops999@ymail.com>
2012-10-18 20:14:19 -0700

    Indentation stuff

--- src/thesbros/bukkit/CFBanner/CFBanner.java
+++ src/thesbros/bukkit/CFBanner/CFBanner.java
@@ -16,47 +16,53 @@ import org.bukkit.plugin.java.JavaPlugin;
  * @author opspwns, CainFool
  */
 //Start class
-public class CFBanner extends JavaPlugin implements Listener {
+public class CFBanner extends JavaPlugin implements Listener
+	{
 	//Start setting variables
 	Logger log = Logger.getLogger("Minecraft");
 	//End setting variables
 	//Start enable code
-	public void onEnable(){
+	public void onEnable()
+	{
 		if(this.getConfig().getBoolean("enabled") == true)
 		{
 		this.getConfig().options().copyDefaults(true);
-		if(this.getConfig().getBoolean("autoUpdate") == true){
+		if(this.getConfig().getBoolean("autoUpdate") == true)
+		{
 			Updater updater = new Updater(this, this.getName().toLowerCase(), this.getFile(), Updater.UpdateType.DEFAULT, true);
 		}
 		this.saveConfig();
 		//Register the Listener
 		getServer().getPluginManager().registerEvents(this, this);
-		try {
+		try
+		{
 		    Metrics metrics = new Metrics(this);
 		    metrics.start();
-		} catch (IOException e) {
+		}
+		catch (IOException e)
+		{
 		    log.severe("CFBanner: Could not load Metrics!");
 		}
-		//Print stuff
-		log.info("====================");
-		log.info("  CFBanner Enabled  ");
-		log.info("   Made by opspwns  ");
-		log.info("   and CainFoool.   ");
-		log.info("====================");
+			//Print stuff
+			log.info("====================");
+			log.info("  CFBanner Enabled  ");
+			log.info("   Made by opspwns  ");
+			log.info("   and CainFoool.   ");
+			log.info("====================");
 		}
 		else
 		{
-		//Print stuff
-		PluginManager pluginManager = Bukkit.getServer().getPluginManager();
-		pluginManager.disablePlugin(this);
-		log.info("CFBanner: Plugin not enabled! Check config.yml to enable!");
-		log.info("CFBanner: Disabled!");
+			//Print stuff
+			PluginManager pluginManager = Bukkit.getServer().getPluginManager();
+			log.info("CFBanner: Plugin not enabled! Check config.yml to enable!");
+			pluginManager.disablePlugin(this);
 		}
 	}
 	//End enable code
 	
 	//Start disable code
-	public void onDisable(){
+	public void onDisable()
+	{
 		//Print stuff
 		log.info("===================");
 		log.info(" CFBanner Disabled ");
